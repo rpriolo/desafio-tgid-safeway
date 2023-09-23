81f1ba09ea227dde0c2ad69e9584863e5edb1a29
Renato Priolo <rpriolo@gmail.com>
2023-09-22 22:37:18 -0300

    Tratamento de erros em transações

--- src/main/java/br/com/tgid/safeway/infra/TratadorErrosTransacao.java
+++ src/main/java/br/com/tgid/safeway/infra/TratadorErrosTransacao.java
@@ -2,9 +2,13 @@ package br.com.tgid.safeway.infra;
 
 import br.com.tgid.safeway.exception.ExceptionDTO;
 import br.com.tgid.safeway.exception.TransacaoInvalidaException;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.sql.SQLIntegrityConstraintViolationException;
 
 @RestControllerAdvice
 public class TratadorErrosTransacao {
@@ -13,4 +17,20 @@ public class TratadorErrosTransacao {
         ExceptionDTO ex = new ExceptionDTO("O valor da transação precisa ser maior que zero", "400");
         return ResponseEntity.badRequest().body(ex);
     }
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity empresaNaoExiste() {
+        ExceptionDTO ex = new ExceptionDTO("O ID informado não existe na base de dados de empresas", "404");
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex);
+    }
+
+    @ExceptionHandler(SQLIntegrityConstraintViolationException.class)
+    public ResponseEntity clienteNaoExiste() {
+        ExceptionDTO ex = new ExceptionDTO("O ID informado não existe na base de dados de clientes", "404");
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex);
+    }
+
+
+
+
 }
